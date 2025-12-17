e1dbddaba9cf0e74eef4d6a18fca57174564a167
Kaspatou07 <kawap07@gmail.com>
2025-12-17 03:36:40 +0100

    initial commit

---
+++ src/pages/Home/index.test.js
@@ -0,0 +1,60 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import Home from "./index";
+
+
+
+describe("When Form is created", () => {
+  it("a list of fields card is displayed", async () => {
+    render(<Home />);
+    await screen.findByText("Email");
+    await screen.findByText("Nom");
+    await screen.findByText("Prénom");
+    await screen.findByText("Personel / Entreprise");
+  });
+
+  describe("and a click is triggered on the submit button", () => {
+    it("the success message is displayed", async () => {
+      render(<Home />);
+      fireEvent(
+        await screen.findByText("Envoyer"),
+        new MouseEvent("click", {
+          cancelable: true,
+          bubbles: true,
+        })
+      );
+      await screen.findByText("En cours");
+      await screen.findByText("Message envoyé !");
+    });
+  });
+
+});
+
+
+describe("When a page is created", () => {
+  it("a list of events is displayed", async () => {
+    render(<Home />);
+    const eventCards = await screen.findAllByTestId('card-testid');
+  expect(eventCards.length).toBeGreaterThan(0);
+  });
+  
+  it("a list of people is displayed", async () => {
+    render(<Home />);
+    await screen.findByText("Samira");
+    await screen.findByText("Jean-baptiste");
+    await screen.findByText("Alice");
+    await screen.findByText("Luís");
+    await screen.findByText("Christine");
+    await screen.findByText("Isabelle");
+  });
+  
+  it("a footer is displayed", () => {
+    render(<Home />);
+    const footerElement = screen.getByRole('contentinfo');
+    expect(footerElement).toBeInTheDocument();
+  })
+  it("an event card, with the last event, is displayed", async () => {
+   // à compléter
+
+   
+  });
+});
